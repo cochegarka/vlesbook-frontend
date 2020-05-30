ce56e55ab5b673e62b35b946db65dc409012bfae
Anton Zavyalov <megadeathlightsaber@gmail.com>
2020-05-31 00:29:05 +0700

    Create basic layout

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,72 @@
 <script>
-	export let name;
+    import 'bulma/css/bulma.css'
+    import '@fortawesome/fontawesome-free/css/all.css'
+
+    let selected;
+
+    let keyStr = '';
+    let keyBytes;
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<section class="section">
+    <div class="container has-text-centered">
+        <div class="field">
+            <div class="control">
+                <label>
+                    <input class="input" bind:value={keyStr} placeholder="Ключ шифрования">
+                </label>
+            </div>
+        </div>
+        <div class="field">
+            <div class="control">
+                <label>
+                    <input class="input" bind:value={keyBytes} placeholder="Ключ шифрования (байты)">
+                </label>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="section">
+    <div class="container">
+        <div class="columns">
+            <div class="column">
+                <label>
+                    <textarea class="textarea is-info" placeholder="Обычный текст" rows="15"></textarea>
+                </label>
+            </div>
+            <div class="column">
+                <label>
+                    <textarea class="textarea is-success" placeholder="Шифровка" rows="15"></textarea>
+                </label>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="section">
+    <div class="container has-text-centered">
+        <div class="select is-primary">
+            <label>
+                <select bind:value={selected}>
+                    <option>Шифровка</option>
+                    <option>Расшифровка</option>
+                </select>
+            </label>
+        </div>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+        <button class="button is-primary">
+            <!-- И мне не стыдно! -->
+            {#if selected === 'Шифровка'}
+                Зашифровать
+            {:else}
+                Расшифровать
+            {/if}
+        </button>
+    </div>
+</section>
+<footer class="footer">
+    <div class="content has-text-centered">
+        <p>
+            <strong>Передаю привет семье: маме, папе, брату, бабушке, кошке Анфисе.</strong>
+        </p>
+    </div>
+</footer>
